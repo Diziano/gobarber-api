5cf30ee61690c670184022c6a840252ac98f859c
Diziano <dizianomachado@gmail.com>
2020-05-27 23:14:35 -0300

    Tests refactoring

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -42,7 +42,6 @@ class ListProviderAppointmentsService {
           day,
         },
       );
-      console.log('Buscou do banco');
 
       await this.cacheProvider.save(cacheKey, appointments);
     }
